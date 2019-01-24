991cac17a9749ac29ed4847b9626f780a998a728
simphiwe-hlabisa <simphiwemobileapps@gmail.com>
2019-01-24 14:01:51 +0200

    updated

--- lib/GovCategory_list_tile.dart
+++ lib/GovCategory_list_tile.dart
@@ -12,6 +12,180 @@ class GovCategory_list_tile extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Container(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                                            Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                                            SizedBox(
+                        width: 5.0,
+                      ),
+                                                            Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                                            Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                                            SizedBox(
+                        width: 5.0,
+                      ),
+                                                            Expanded(
+                     child: Container(
+                      height: 150.0,
+                       child: new Column(
+                         children: <Widget>[
+                           new Container(
+                             height: 100.0,
+                             decoration: new BoxDecoration(
+                                 borderRadius: new BorderRadius.circular(5.0),
+                                 image: new DecorationImage(
+                                     image: new NetworkImage(
+                                         'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
+                           new Text(
+                             "Play Station",
+                           style: new TextStyle(fontSize: 16.0),
+                             textAlign: TextAlign.center,
+                           )
+                         ],
+                       ),
+                     ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
       // child: ListView.builder(
       //   itemCount: gov_categories.length,
       //   padding: const EdgeInsets.all(0.0),
@@ -44,37 +218,39 @@ class GovCategory_list_tile extends StatelessWidget{
       //     );
       //   },
       // ),
-       color: Colors.white30,
-                 child: GridView.builder(
-                  //remove space in the top
-                  padding: EdgeInsets.only(top: 5.0,bottom: 5.0,left: 5.0,right: 5.0),
-                  shrinkWrap: false,
-                  primary: true,
-                  itemCount: gov_categories.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 1.0,crossAxisSpacing: 4.0,mainAxisSpacing: 4.0),
-                  itemBuilder: (BuildContext context, int index){
-                    return                   new Expanded(
-                    child: Container(
-                      height: 150.0,
-                      child: new Column(
-                        children: <Widget>[
-                          new Container(
-                            height: 100.0,
-                            decoration: new BoxDecoration(
-                                borderRadius: new BorderRadius.circular(5.0),
-                                image: new DecorationImage(
-                                    image: new NetworkImage(
-                                        'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
-                                    fit: BoxFit.cover)),
-                          ),
-                          new Text(
-                            "Play Station",
-                            style: new TextStyle(fontSize: 16.0),
-                            textAlign: TextAlign.center,
-                          )
-                        ],
-                      ),
-                    ),
+      //  color: Colors.white30,
+      //            child: GridView.builder(
+      //             //remove space in the top
+      //             padding: EdgeInsets.only(top: 5.0,bottom: 5.0,left: 5.0,right: 5.0),
+      //             shrinkWrap: false,
+      //             primary: true,
+      //             itemCount: gov_categories.length,
+      //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 1.0,crossAxisSpacing: 4.0,mainAxisSpacing: 4.0),
+      //             itemBuilder: (BuildContext context, int index){
+                    // return  GestureDetector(
+                    //   child: Container(
+                    //     child: new Expanded(
+                    // child: Container(
+                    //   height: 150.0,
+                    //   child: new Column(
+                    //     children: <Widget>[
+                    //       new Container(
+                    //         height: 100.0,
+                    //         decoration: new BoxDecoration(
+                    //             borderRadius: new BorderRadius.circular(5.0),
+                    //             image: new DecorationImage(
+                    //                 image: new NetworkImage(
+                    //                     'https://www.howtogeek.com/wp-content/uploads/2016/01/steam-and-xbox-controllers.jpg'),
+                    //                 fit: BoxFit.cover)),
+                    //       ),
+                    //       new Text(
+                    //         "Play Station",
+                    //         style: new TextStyle(fontSize: 16.0),
+                    //         textAlign: TextAlign.center,
+                    //       )
+                    //     ],
+                    //   ),
+                    // ),
 
                     // return GridTile(
                     //   child: Card(
@@ -115,10 +291,14 @@ class GovCategory_list_tile extends StatelessWidget{
                       //   ),
                       //   color: Colors.white,
                       //   //padding: EdgeInsets.all(1.0),
-                      //   onPressed: (){},) 
-                    );
-                  },
-                )
+                    //   //   onPressed: (){},) 
+                    // ),
+                    //   )
+                      
+                    // );                
+                     
+                  //},
+                //)
     );
   }
 
