975f8a6a3e28f2579015970de263fc0a01b9f4d5
simphiwe sifiso hlabisa <sfisohlabisa@gmail.com>
2019-02-01 12:20:15 +0200

    updated

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:validate/validate.dart';
 import 'package:http/http.dart' as http;
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
